0f9aaf8c157647d5210f8109456fa5fe46254d83
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-28 17:14:29 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout (string) {
+  'Hello!'.toUpperCase()
+  return string.toUpperCase()
+}
+function whisper (string){
+  "Hello!".toLowerCase()
+  return string.toLowerCase()
+}
+function logShout (string){
+  const spy="hello".toUpperCase()
+  console.log(spy)
+}
+function logWhisper (string){
+  const spy="HELLO".toLowerCase()
+  console.log(spy)
+}
+function sayHiToGrandma (string){
+  var lowercase='hello'.toLowerCase()
+  if (lowercase.toLowerCase() === lowercase){
+  return "I can't hear you!";
+}
+}
+function sayHiToGrandma (string){
+  var uppercase = "HELLO!".toUpperCase()
+ if (uppercase.toUpperCase() === uppercase){
+  return "YES INDEED";
+}
+}
+function sayHiToGrandma (string){
+  "I love you, Grandma."
+ if (uppercase.toUpperCase() === uppercase){
+  return "I love you,too.";
+}
+}
