c81ed0e315519eded528f873aa6401bd0f421740
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-28 17:38:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper (string){
 
 function sayHiToGrandma (string){
   'hello'.toLowerCase()
-  if (lowercase.toLowerCase() === lowercase){
+  if (string.toLowerCase() === lowercase){
   return "I can't hear you!";
 }
 }
